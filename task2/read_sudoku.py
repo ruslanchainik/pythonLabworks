a420c8b385103a4df89ac83a16cc40cf6df15f58
Ruslan Zhemlikhanov <ruslan@milayasobachka.ru>
2025-07-11 18:40:56 +0300

    Реализованы get_block и find_empty_positions

--- task2/read_sudoku.py
+++ task2/read_sudoku.py
@@ -9,4 +9,40 @@ def read_sudoku(filename):
     return lines
 
 grid = read_sudoku('sudoku.txt')
-print(grid)
+
+
+def group(values, n):
+    return [values[i:i+n] for i in range(0, len(values), n)]
+
+
+def get_row(grid, pos):
+    return grid[pos[0]]
+
+def get_col(grid, pos):
+    col = []
+    for i in range(len(grid)):
+        col.append(grid[i][pos[1]])
+    return col
+
+def get_block(grid, pos):
+    row_start = (pos[0] // 3) * 3
+    col_start = (pos[1] // 3) * 3
+    block = []
+    for i in range(row_start, row_start + 3):     
+        for k in range(col_start, col_start + 3):  
+            block.append(grid[i][k])
+    return block
+
+def find_empty_positions(grid):
+    for i in range(len(grid)):
+        for k in range(len(grid[i])):
+            if grid[i][k] =='.':
+                return (i, k)
+    
+    return None
+                
+                
+            
+        
+
+        
